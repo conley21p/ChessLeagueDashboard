d003a722973bd078ed6e04aed5e24b4e3f3637f2
conley21p <conley21p@gmial.com>
2023-04-30 12:48:29 -0500

    Standings page, game view, register player

---
+++ ChessLeague/Data/Game.cs
@@ -0,0 +1,9 @@
+
+public class Game
+{
+    public uint Player1         { get; set; }   = 0;
+    public uint Player2         { get; set; }   = 0;
+    public uint Winner          { get; set; }   = 0;                // uint is the players ID
+    public bool Draw            { get; set; }   = false;
+    public String PGN           { get; set; }   = string.Empty;
+}
--- ChessLeague/Data/Player.cs
+++ ChessLeague/Data/Player.cs
@@ -1,6 +1,10 @@
 public class Player
 {
-    public int ID { get; set; }
+    public uint ID { get; set; }
     public string Name { get; set; } = string.Empty;
     public int Rating { get; set; }
+    public int HighestRating { get; set; }
+    public int Wins { get; set; }
+    public int Losses { get; set; }
+    public int Draws { get; set; }
 }
