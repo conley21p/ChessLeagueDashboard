6821e1f17f2957c9dffd182e7d710db6e3c14f42
conley21p <conley21p@gmial.com>
2023-05-08 18:15:42 -0500

    Update create game long for new game object, allow scheduled games as well

--- ChessLeague/Data/Game.cs
+++ ChessLeague/Data/Game.cs
@@ -1,10 +1,13 @@
 
 public class Game
 {
-    public uint Player1         { get; set; }   = 0;
-    public uint Player2         { get; set; }   = 0;
-    public uint Winner          { get; set; }   = 0;                // uint is the players ID
-    public bool Draw            { get; set; }   = false;
-    public String PGN           { get; set; }   = string.Empty;
+    public uint SeasonId            { get; set; }   = 0;
+    public uint TournamentId        { get; set; }   = 0;
+    public uint Light               { get; set; }   = 0;
+    public uint Dark                { get; set; }   = 0;
+    public uint State               { get; set; }   = 0;                // uint is the players ID
+    public String PGN               { get; set; }   = string.Empty;
+    public String Desc               { get; set; }   = string.Empty;
+    public String TimePlayed         { get; set; }   = string.Empty;
 
 }
---
+++ ChessLeague/Data/Season.cs
@@ -0,0 +1,8 @@
+
+public class Season
+{
+    public uint Name            { get; set; }   = 0;
+    public String StartDate     { get; set; }   = string.Empty;
+    public String EndDate       { get; set; }   = string.Empty;
+
+}
