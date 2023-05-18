1bf46800ca660ded3dc13cb08ed667cf1a54b13c
conley21p <conley21p@gmial.com>
2023-05-17 22:13:36 -0500

    Create Game, add player to season working!! Create Season may not be working

--- ChessLeague/Data/Season.cs
+++ ChessLeague/Data/Season.cs
@@ -1,7 +1,7 @@
 
 public class Season
 {
-    public int ID { get; set; } = 0;
+    public uint ID { get; set; } = 0;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     public DateTime? DeletedAt { get; set; } = null;
@@ -9,5 +9,12 @@ public class Season
     public DateTime StartDate { get; set; } = DateTime.UtcNow;
     public DateTime EndDate { get; set; } = DateTime.UtcNow;
     public int TournamentId { get; set; } = 0;
+
+}
+public class SeasonCreate
+{
+    public string Name { get; set; } = "";
+    public string StartDate { get; set; }
+    public string EndDate { get; set; } 
 
 }
