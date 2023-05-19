be7f11156b38e87dac8643e09c2709d5000b2881
conley21p <conley21p@gmial.com>
2023-05-18 22:14:03 -0500

    All good except backend

--- ChessLeague/Data/Season.cs
+++ ChessLeague/Data/Season.cs
@@ -5,7 +5,7 @@ public class Season
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     public DateTime? DeletedAt { get; set; } = null;
-    public string Name { get; set; } = string.Empty;
+    public string Name { get; set; } = "";
     public DateTime StartDate { get; set; } = DateTime.UtcNow;
     public DateTime EndDate { get; set; } = DateTime.UtcNow;
     public int TournamentId { get; set; } = 0;
