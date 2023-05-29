494690c4587b36247325b08ff4daf95696e39936
conley21p <conley21p@gmial.com>
2023-05-29 11:53:38 -0500

    Mouse Idle finished, need to fix the select player page in create game window.

--- ChessLeague/wwwroot/js/mouseIdle.js
+++ ChessLeague/wwwroot/js/mouseIdle.js
@@ -3,12 +3,13 @@ let idleTimeout;
 function handleMouseIdle() {
   // Hide buttons or perform any desired action
     console.log("hiding display");
-  // document.getElementById('buttons-menu').style.display = 'none';
+  document.getElementById('buttons-menu').style.display = 'none';
 }
 
 function resetIdleTimeout() {
   clearTimeout(idleTimeout);
-  idleTimeout = setTimeout(handleMouseIdle, 30000); // 30 seconds (30000 milliseconds)
+  idleTimeout = setTimeout(handleMouseIdle, 10000); // 30 seconds (30000 milliseconds)
+  document.getElementById('buttons-menu').style.display = 'flex';
 }
 
 document.addEventListener('mousemove', resetIdleTimeout);
